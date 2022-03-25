00954ab17a35584f1de3d467bc5059088b6b4d6b
Arthur Bourbousson <abourbou@z4r8p3.42lyon.fr>
2022-03-25 11:35:14 +0100

    level04-05

---
+++ level01/Ressources/script.sh
@@ -0,0 +1,2 @@
+find / -user flag01 2>/dev/null
+cat etc/passwd
---
+++ level05/Ressources/script.sh
@@ -0,0 +1,4 @@
+cat /var/mail/level05
+cat /usr/sbin/openarenaserver
+cat /opt/openarenaserver
+echo "getflag > /tmp/flag" > exec_getflag
